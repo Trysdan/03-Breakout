83f8150657a1fbc5b6bff6222b7eb45cd44420ff
Ramón José Belandria Quintero <belandriaquinteroramonjose@gmail.com>
2025-03-22 17:29:01 -0400

    New sprite for StickyPaddle power upp

--- src/powerups/StickyPaddle.py
+++ src/powerups/StickyPaddle.py
@@ -6,7 +6,7 @@ from src.powerups.PowerUp import PowerUp
 class StickyPaddle(PowerUp):
 
     def __init__(self, x: int, y: int) -> None:
-        super().__init__(x, y, 2)
+        super().__init__(x, y, 4)
 
     def take(self, play_state: TypeVar("PlayState")) -> None:
         settings.SOUNDS["paddle_hit"].stop()
